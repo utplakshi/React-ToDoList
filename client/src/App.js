110856d0ad32efaaef7588a16b9aa410a9703e9f
utplakshi <71188736+utplakshi@users.noreply.github.com>
2021-12-17 13:22:05 +0530

    Final Todo List

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
+import React from 'react';
 import './App.css';
-import TodoForm from './Components/TodoForm';
 import TodoList from './Components/TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <TodoList/>
+    <div className='todo-app'>
+      <TodoList />
     </div>
   );
 }
