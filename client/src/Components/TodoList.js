5f0f188229e7c0d21d24126cd795ab70a86f6f76
utplakshi <71188736+utplakshi@users.noreply.github.com>
2021-12-17 23:09:31 +0530

    Using Local Storage

--- client/src/Components/TodoList.js
+++ client/src/Components/TodoList.js
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoForm from './TodoForm';
 import Todo from './Todo';
 
 function TodoList() {
+  let d = localStorage.getItem('data');
+  if(d==undefined || d==null) 
+    localStorage.setItem('data',[]);
+
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('data'));
+    if (items) {
+      setTodos(items);
+    }
+  }, []);
 
   const addTodo = todo => {
     if (!todo.text || /^\s*$/.test(todo.text)) {
       return;
-    }
-
+    } 
     const newTodos = [todo, ...todos];
-
+    localStorage.setItem('data', JSON.stringify(newTodos));
     setTodos(newTodos);
     console.log(...todos);
   };
